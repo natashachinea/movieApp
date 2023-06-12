6f0f62a75b661dc83c0509a264a6fc3e28bf76c0
Natasha <112275600+natashachinea@users.noreply.github.com>
2023-06-12 12:46:14 +0100

    Image-component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,6 +1,6 @@
  function Image({src, alt, ...others}) {
     const notFound = <img src='images/Screenshot 2023-06-07 at 17.33.58.png' alt="not found" />;
-    const image=
+
      return (
 
          <div>
